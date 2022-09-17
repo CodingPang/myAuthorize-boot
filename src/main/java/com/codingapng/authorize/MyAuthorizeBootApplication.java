9b5e3ba971b1a275db47820d430232533edd3818
CodingPang <2292038494@qq.com>
2022-09-17 17:31:26 +0800

    空项目的建立

---
+++ src/main/java/com/codingapng/authorize/MyAuthorizeBootApplication.java
@@ -0,0 +1,13 @@
+package com.codingapng.authorize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAuthorizeBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAuthorizeBootApplication.class, args);
+    }
+
+}
